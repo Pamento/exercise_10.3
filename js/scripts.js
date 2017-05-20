52459215c4ea46ffcdd988b0cd3e3eb86973ebd2
Pamento <pamento@op.pl>
2017-05-21 00:59:31 +0200

    put jQuery controls out of master function

--- js/scripts.js
+++ js/scripts.js
@@ -1,62 +1,46 @@
 $(function(){
 
 	var carouselList = $("#carousel ul");
-	var slides = carouselList.find('li');
-	var slideLoop = 1;
 	var interval;
-
-	var goLeft = $('#leftDirect');			// for the arrow left
-	var goRight = $('#rightDirect');		// for the arrow right
-	var lookEachSlide = $('#outSlider');
+	var lookEachSlide = $("#outSlider");
 
 	function goMoveSlider() {
 		interval = setInterval(function(){
-
-			carouselList.animate({'margin-left': '-=400px'}, 700, moveSlide);
+			carouselList.animate({"marginLeft": -400}, 700, moveSlide);
 		},3000);
-
 		function moveSlide(){
-			slideLoop++;
-			if (slideLoop === slides.length) {
-				slideLoop = 1;
-				carouselList.css('margin-left', 0);
-			}
+			var firstItem = carouselList.find("li:first");
+			var lastItem = carouselList.find("li:last");
+			lastItem.after(firstItem);
+			carouselList.css({marginLeft:0});
 		}
 	}
-
 	function stopMoveSlider() {
 		clearInterval(interval);
 	}
-
-	lookEachSlide.on('mouseenter', stopMoveSlider).on('mouseleave', goMoveSlider);
+	lookEachSlide.on("mouseenter", stopMoveSlider).on("mouseleave", goMoveSlider);
 	goMoveSlider();
-
-	goLeft.click(function() {
-		carouselList.animate({'margin-left': '-=400px'}, 700, function(){
-			slideLoop++;
-			if (slideLoop === slides.length) {
-				slideLoop = 1;
-				carouselList.css('margin-left', 0);
-			}
-		});
-	});
-
-	goRight.click(function() {
-		if(slideLoop == 1) {
-			slideLoop = 6;
-			carouselList.css('margin-left', '-2000px');
-		}
-		carouselList.animate({'margin-left': '+=400px'}, 700, function(){
-			slideLoop--;
-		});
-	});
-
-
 });
 
 
+$("#leftDirect").click(function(event) {
+	event.preventDefault();
+	var carouselList = $("#carousel ul");
+	carouselList.animate({"marginLeft": -400}, 700, function(){
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	});
+});
 
+$("#rightDirect").click(function(event) {
+	event.preventDefault();
+	var carouselList = $("#carousel ul");
+	var lastItem = carouselList.find("li:last");
+	var firstItem = carouselList.find("li:first");
+	firstItem.before(lastItem);
 
-
-
-
+	carouselList.animate({"marginRight": 400}, 700);
+	carouselList.css({marginLeft:0});
+});
